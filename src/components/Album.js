2627609181b5ee31b7bd84cb25ee289dfcb8887f
Daniel Halterman <drhalterman1@yahoo.com>
2017-12-27 00:40:10 -0500

    Added forward button

--- src/components/Album.js
+++ src/components/Album.js
@@ -52,6 +52,14 @@ class Album extends Component {
     this.play(newSong);
   }
 
+  handleNextClick(song) {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
   render() {
     return (
       <section className="album">
@@ -90,6 +98,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
